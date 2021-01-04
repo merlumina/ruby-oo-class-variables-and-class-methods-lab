f5ea2292ef5ca1ed9b9653749728ac9280d2a7f1
Liz <lizriderwilson@gmail.com>
2021-01-04 12:29:54 -0500

    commit tree db358c5cdbaca44d1c530d0a94c4ca95bfc6d98a

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name, @artist, @genre = name, artist, genre
+        @@count += 1
+        @@artists << @artist
+        @@genres << @genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.artist_count
+        artist_count_hash = Hash.new(0)
+        @@artists.each do |artist|
+            artist_count_hash[artist] += 1
+        end
+        artist_count_hash
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        genre_count_hash = Hash.new(0)
+        @@genres.each do |genre|
+            genre_count_hash[genre] += 1
+        end
+        genre_count_hash
+    end
+
+end
